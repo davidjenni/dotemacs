be878053af8467702ebd37a052b8e4ac123e5f30
david@david-mbp <david@davidjenni.com>
2014-04-20 21:59:47 +0800

    emacs dot files with Evil vim emulation
    adapted from https://github.com/bling/dotemacs/

---
+++ config/init-projectile.el
@@ -0,0 +1,28 @@
+(require-package 'projectile)
+
+
+(setq projectile-cache-file (concat user-emacs-directory ".cache/projectile.cache"))
+(setq projectile-known-projects-file (concat user-emacs-directory ".cache/projectile-bookmarks.eld"))
+(setq projectile-indexing-method 'alien)
+(setq projectile-use-git-grep t)
+
+
+(require 'projectile)
+
+
+(add-to-list 'projectile-globally-ignored-directories "elpa")
+(add-to-list 'projectile-globally-ignored-directories ".cache")
+(add-to-list 'projectile-globally-ignored-directories "node_modules")
+
+
+(when (executable-find "ack")
+  (require-package 's)
+  (let ((val (concat "ack -f --print0" (s-join " --ignore-dir=" (cons "" projectile-globally-ignored-directories)))))
+    (setq projectile-generic-command val)
+    (setq projectile-svn-command val)))
+
+
+(projectile-global-mode t)
+
+
+(provide 'init-projectile)
---
+++ init.el
@@ -0,0 +1,34 @@
+(when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(unless (display-graphic-p) (menu-bar-mode -1))
+
+(load-theme 'misterioso)
+
+(add-to-list 'load-path (concat user-emacs-directory "config"))
+
+(require 'cl)
+(require 'init-packages)
+
+(defgroup dotemacs nil
+  "Custom configuration for dotemacs."
+  :group 'local)
+
+(defcustom dotemacs-modules
+  '(init-core
+    init-eyecandy
+
+    init-projectile
+    init-ido
+
+    ; init-vcs
+
+    init-misc
+    init-evil
+    ; init-bindings)
+   )
+  "Set of modules enabled in dotemacs."
+  :group 'dotemacs)
+
+(dolist (module dotemacs-modules)
+  (require module))
+
